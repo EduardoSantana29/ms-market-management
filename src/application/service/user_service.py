dcc94ce8f061b808dd6916ebe1d7c97306cfac71
Vinicius_Oliveira <viniciuslimaoliver18@gmail.com>
2025-03-25 23:17:10 -0300

    Implementar a lógica para cadastrar um vendedor no Banco de Dados

---
+++ src/application/service/user_service.py
@@ -0,0 +1,31 @@
+from src.infrastructure.model.user import User
+from src.infrastructure.http.whatsapp import WhatsAppService
+from src.config.data_base import db
+import random
+import jwt
+from datetime import datetime, timedelta
+
+class UserService:
+    """Classe que gerencia o serviço de usuários da aplicação."""
+
+    @staticmethod
+    def create_seller(nome, cnpj, email, celular, senha):
+        """Cadastra o usuário no banco de dados.\n
+        Valida se o usuário não foi cadastrado anteriormente, à partir do e-mail.\n
+        Gera um código de ativação randômico e envia à partir da instância do serviço do WhatsApp."""
+
+        existing_seller = User.query.filter_by(email=email).first()
+        if existing_seller:
+            raise Exception("E-mail já cadastrado")
+
+        activation_code = str(random.randint(1000, 9999))
+        new_seller = User(name=nome, cnpj=cnpj, email=email, celular=celular)
+        new_seller.set_password(senha)  
+        new_seller.activation_code = activation_code
+
+        db.session.add(new_seller)
+        db.session.commit()
+
+        WhatsAppService.send_code(celular, activation_code)
+
+        return new_seller
