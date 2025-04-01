20f7091b99422994178383e6524d7562fbaa63fb
nicholascostap <nickxxtreme@gmail.com>
2025-04-01 19:32:21 -0300

    :wrench: config(run): definindo configurações do projeto para renderizar os templates e integrar com API

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from src.config.data_base import init_db, db
 from src.route import init_routes
+from src.route import blueprint
 import os
 
 def create_app():
     """Inicializa a aplicação Flask e a base de dados SQLite."""
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
     init_db(app)
     init_routes(app)
 
@@ -14,9 +15,15 @@ def create_app():
             db.create_all()
             print("📌 Banco de dados SQLite criado!")
 
+    app.register_blueprint(blueprint, url_prefix='/api')
+
     return app
 
 app = create_app()
+
+@app.route('/')
+def index():
+    return render_template('index.html', output="")
 
 if __name__ == '__main__':
     app.run(debug=True)
