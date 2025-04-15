3971b42e8f5dd08039b498c627a7612dabc68c27
gabrieldevrs <gabriel.gislainevida@gmail.co>
2025-04-15 19:48:07 -0300

    Implementação da Pagina de Gerenciamento de Produtos

--- run.py
+++ run.py
@@ -36,5 +36,9 @@ def activate():
 def login_page():
     return render_template('login.html')
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
